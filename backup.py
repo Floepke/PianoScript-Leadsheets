ad5139c90f1d7e6232806d7f85b14890b7d4aea1
philipbergwerf <philipbergwerf@gmail.com>
2020-10-02 21:43:53 +0200

    q

---
+++ backup.py
@@ -0,0 +1,336 @@
+######################################### Octave 0
+        # A0
+        if 1 in all_notes:
+            white_key(15+x, 25+y, 5, root.CanvasPage)
+        # a0
+        if 2 in all_notes:
+            black_key(20+x, 15+y, 5, root.CanvasPage)
+        # B0
+        if 3 in all_notes and 4 in all_notes:
+            small_white_key(20+x, 20+y)
+        if 3 in all_notes and 4 not in all_notes:
+            white_key(25+x, 25+y, 5, root.CanvasPage)
+        ######################################### Octave 1
+        # C1
+        if 4 in all_notes and 3 in all_notes:
+            small_white_key(27.5+x, 20+y)
+        if 4 in all_notes and 3 not in all_notes:
+            white_key(30+x, 25+y, 5, root.CanvasPage)
+        # c1
+        if 5 in all_notes:
+            black_key(35+x, 15+y, 5, root.CanvasPage)
+        # D1
+        if 6 in all_notes:
+            white_key(40+x, 25+y, 5, root.CanvasPage)
+        # d1
+        if 7 in all_notes:
+            black_key(45+x, 15+y, 5, root.CanvasPage)
+        # E1
+        if 8 in all_notes and 9 in all_notes:
+            small_white_key(45+x, 20+y)
+        if 8 in all_notes and 9 not in all_notes:
+            white_key(50+x, 25+y, 5, root.CanvasPage)
+        # F1
+        if 9 in all_notes and 8 in all_notes:
+            small_white_key(52.5+x, 20+y)
+        if 9 in all_notes and 8 not in all_notes:
+            white_key(55+x, 25+y, 5, root.CanvasPage)
+        # f1
+        if 10 in all_notes:
+            black_key(60+x, 15+y, 5, root.CanvasPage)
+        # G1
+        if 11 in all_notes:
+            white_key(65+x, 25+y, 5, root.CanvasPage)
+        # g1
+        if 12 in all_notes:
+            black_key(70+x, 15+y, 5, root.CanvasPage)
+        # A1
+        if 13 in all_notes:
+            white_key(75+x, 25+y, 5, root.CanvasPage)
+        # a1
+        if 14 in all_notes:
+            black_key(80+x, 15+y, 5, root.CanvasPage)
+        # B1
+        if 15 in all_notes and 16 in all_notes:
+            small_white_key(80+x, 20+y)
+        if 15 in all_notes and 16 not in all_notes:
+            white_key(85+x, 25+y, 5, root.CanvasPage)
+        ######################################### Octave 2
+        # C2
+        if 16 in all_notes and 15 in all_notes:
+            small_white_key(87.5+x, 20+y)
+        if 16 in all_notes and 15 not in all_notes:
+            white_key(90+x, 25+y, 5, root.CanvasPage)
+        # c2
+        if 17 in all_notes:
+            black_key(95+x, 15+y, 5, root.CanvasPage)
+        # D2
+        if 18 in all_notes:
+            white_key(100+x, 25+y, 5, root.CanvasPage)
+        # d2
+        if 19 in all_notes:
+            black_key(105+x, 15+y, 5, root.CanvasPage)
+        # E2
+        if 20 in all_notes and 21 in all_notes:
+            small_white_key(105+x, 20+y)
+        if 20 in all_notes and 21 not in all_notes:
+            white_key(110+x, 25+y, 5, root.CanvasPage)
+        # F2
+        if 21 in all_notes and 20 in all_notes:
+            small_white_key(112.5+x, 20+y)
+        if 21 in all_notes and 20 not in all_notes:
+            white_key(115+x, 25+y, 5, root.CanvasPage)
+        # f2
+        if 22 in all_notes:
+            black_key(120+x, 15+y, 5, root.CanvasPage)
+        # G2
+        if 23 in all_notes:
+            white_key(125+x, 25+y, 5, root.CanvasPage)
+        # g2
+        if 24 in all_notes:
+            black_key(130+x, 15+y, 5, root.CanvasPage)
+        # A2
+        if 25 in all_notes:
+            white_key(135+x, 25+y, 5, root.CanvasPage)
+        # a2
+        if 26 in all_notes:
+            black_key(140+x, 15+y, 5, root.CanvasPage)
+        # B2
+        if 27 in all_notes and 28 in all_notes:
+            small_white_key(140+x, 20+y)
+        if 27 in all_notes and 28 not in all_notes:
+            white_key(145+x, 25+y, 5, root.CanvasPage)
+        ##################################### Octave 3
+        # C3
+        if 28 in all_notes and 27 in all_notes:
+            small_white_key(147.5+x, 20+y)
+        if 28 in all_notes and 27 not in all_notes:
+            white_key(150+x, 25+y, 5, root.CanvasPage)
+        # c3
+        if 29 in all_notes:
+            black_key(155+x, 15+y, 5, root.CanvasPage)
+        # D3
+        if 30 in all_notes:
+            white_key(160+x, 25+y, 5, root.CanvasPage)
+        # d3
+        if 31 in all_notes:
+            black_key(165+x, 15+y, 5, root.CanvasPage)
+        # E3
+        if 32 in all_notes and 33 in all_notes:
+            small_white_key(165+x, 20+y)
+        if 32 in all_notes and 33 not in all_notes:
+            white_key(170+x, 25+y, 5, root.CanvasPage)
+        # F3
+        if 33 in all_notes and 32 in all_notes:
+            small_white_key(172.5+x, 20+y)
+        if 33 in all_notes and 32 not in all_notes:
+            white_key(175+x, 25+y, 5, root.CanvasPage)
+        # f3
+        if 34 in all_notes:
+            black_key(180+x, 15+y, 5, root.CanvasPage)
+        # G3
+        if 35 in all_notes:
+            white_key(185+x, 25+y, 5, root.CanvasPage)
+        # g3
+        if 36 in all_notes:
+            black_key(190+x, 15+y, 5, root.CanvasPage)
+        # A3
+        if 37 in all_notes:
+            white_key(195+x, 25+y, 5, root.CanvasPage)
+        # a3
+        if 38 in all_notes:
+            black_key(200+x, 15+y, 5, root.CanvasPage)
+        # B3
+        if 39 in all_notes and 40 in all_notes:
+            small_white_key(200+x, 20+y)
+        if 39 in all_notes and 40 not in all_notes:
+            white_key(205+x, 25+y, 5, root.CanvasPage)
+
+        ##################################### Octave 4
+        # C4
+        if 40 in all_notes and 39 in all_notes:
+            small_white_key(207.5+x, 20+y)
+        if 40 in all_notes and 39 not in all_notes:
+            white_key(210+x, 25+y, 5, root.CanvasPage)
+        # c4
+        if 41 in all_notes:
+            black_key(215+x, 15+y, 5, root.CanvasPage)
+        # D4
+        if 42 in all_notes:
+            white_key(220+x, 25+y, 5, root.CanvasPage)
+        # d4
+        if 43 in all_notes:
+            black_key(225+x, 15+y, 5, root.CanvasPage)
+        # E4
+        if 44 in all_notes and 45 in all_notes:
+            small_white_key(225+x, 20+y)
+        if 44 in all_notes and 45 not in all_notes:
+            white_key(230+x, 25+y, 5, root.CanvasPage)
+        # F4
+        if 45 in all_notes and 44 in all_notes:
+            small_white_key(232.5+x, 20+y)
+        if 45 in all_notes and 44 not in all_notes:
+            white_key(235+x, 25+y, 5, root.CanvasPage)
+        # f4
+        if 46 in all_notes:
+            black_key(240+x, 15+y, 5, root.CanvasPage)
+        # G4
+        if 47 in all_notes:
+            white_key(245+x, 25+y, 5, root.CanvasPage)
+        # g4
+        if 48 in all_notes:
+            black_key(250+x, 15+y, 5, root.CanvasPage)
+        # A4
+        if 49 in all_notes:
+            white_key(255+x, 25+y, 5, root.CanvasPage)
+        # a4
+        if 50 in all_notes:
+            black_key(260+x, 15+y, 5, root.CanvasPage)
+        # B4
+        if 51 in all_notes and 52 in all_notes:
+            small_white_key(260+x, 20+y)
+        if 51 in all_notes and 52 not in all_notes:
+            white_key(265+x, 25+y, 5, root.CanvasPage)
+
+        ##################################### Octave 5
+        # C5
+        if 52 in all_notes and 51 in all_notes:
+            small_white_key(267.5+x, 20+y)
+        if 52 in all_notes and 51 not in all_notes:
+            white_key(270+x, 25+y, 5, root.CanvasPage)
+        # c5
+        if 53 in all_notes:
+            black_key(275+x, 15+y, 5, root.CanvasPage)
+        # D5
+        if 54 in all_notes:
+            white_key(280+x, 25+y, 5, root.CanvasPage)
+        # d5
+        if 55 in all_notes:
+            black_key(285+x, 15+y, 5, root.CanvasPage)
+        # E5
+        if 56 in all_notes and 57 in all_notes:
+            small_white_key(285+x, 20+y)
+        if 56 in all_notes and 57 not in all_notes:
+            white_key(290+x, 25+y, 5, root.CanvasPage)
+        # F5
+        if 57 in all_notes and 56 in all_notes:
+            small_white_key(292.5+x, 20+y)
+        if 57 in all_notes and 56 not in all_notes:
+            white_key(295+x, 25+y, 5, root.CanvasPage)
+        # f5
+        if 58 in all_notes:
+            black_key(300+x, 15+y, 5, root.CanvasPage)
+        # G5
+        if 59 in all_notes:
+            white_key(305+x, 25+y, 5, root.CanvasPage)
+        # g5
+        if 60 in all_notes:
+            black_key(310+x, 15+y, 5, root.CanvasPage)
+        # A5
+        if 61 in all_notes:
+            white_key(315+x, 25+y, 5, root.CanvasPage)
+        # a5
+        if 62 in all_notes:
+            black_key(320+x, 15+y, 5, root.CanvasPage)
+        # B5
+        if 63 in all_notes and 64 in all_notes:
+            small_white_key(320+x, 20+y)
+        if 63 in all_notes and 64 not in all_notes:
+            white_key(325+x, 25+y, 5, root.CanvasPage)
+
+        ##################################### Octave 6
+        # C6
+        if 64 in all_notes and 63 in all_notes:
+            small_white_key(327.5+x, 20+y)
+        if 64 in all_notes and 63 not in all_notes:
+            white_key(330+x, 25+y, 5, root.CanvasPage)
+        # c6
+        if 65 in all_notes:
+            black_key(335+x, 15+y, 5, root.CanvasPage)
+        # D6
+        if 66 in all_notes:
+            white_key(340+x, 25+y, 5, root.CanvasPage)
+        # d6
+        if 67 in all_notes:
+            black_key(345+x, 15+y, 5, root.CanvasPage)
+        # E6
+        if 68 in all_notes and 69 in all_notes:
+            small_white_key(345+x, 20+y)
+        if 68 in all_notes and 69 not in all_notes:
+            white_key(350+x, 25+y, 5, root.CanvasPage)
+        # F6
+        if 69 in all_notes and 68 in all_notes:
+            small_white_key(352.5+x, 20+y)
+        if 69 in all_notes and 68 not in all_notes:
+            white_key(355+x, 25+y, 5, root.CanvasPage)
+        # f6
+        if 70 in all_notes:
+            black_key(360+x, 15+y, 5, root.CanvasPage)
+        # G6
+        if 71 in all_notes:
+            white_key(365+x, 25+y, 5, root.CanvasPage)
+        # g6
+        if 72 in all_notes:
+            black_key(370+x, 15+y, 5, root.CanvasPage)
+        # A6
+        if 73 in all_notes:
+            white_key(375+x, 25+y, 5, root.CanvasPage)
+        # a6
+        if 74 in all_notes:
+            black_key(380+x, 15+y, 5, root.CanvasPage)
+        # B6
+        if 75 in all_notes and 76 in all_notes:
+            small_white_key(380+x, 20+y)
+        if 75 in all_notes and 76 not in all_notes:
+            white_key(385+x, 25+y, 5, root.CanvasPage)
+
+        ##################################### Octave 7
+        # C7
+        if 76 in all_notes and 75 in all_notes:
+            small_white_key(387.5+x, 20+y)
+        if 76 in all_notes and 75 not in all_notes:
+            white_key(390+x, 25+y, 5, root.CanvasPage)
+        # c7
+        if 77 in all_notes:
+            black_key(395+x, 15+y, 5, root.CanvasPage)
+        # D7
+        if 78 in all_notes:
+            white_key(400+x, 25+y, 5, root.CanvasPage)
+        # d7
+        if 79 in all_notes:
+            black_key(405+x, 15+y, 5, root.CanvasPage)
+        # E7
+        if 80 in all_notes and 81 in all_notes:
+            small_white_key(405+x, 20+y)
+        if 80 in all_notes and 81 not in all_notes:
+            white_key(410+x, 25+y, 5, root.CanvasPage)
+        # F7
+        if 81 in all_notes and 80 in all_notes:
+            small_white_key(412.5+x, 20+y)
+        if 81 in all_notes and 80 not in all_notes:
+            white_key(415+x, 25+y, 5, root.CanvasPage)
+        # f7
+        if 82 in all_notes:
+            black_key(420+x, 15+y, 5, root.CanvasPage)
+        # G7
+        if 83 in all_notes:
+            white_key(425+x, 25+y, 5, root.CanvasPage)
+        # g7
+        if 84 in all_notes:
+            black_key(430+x, 15+y, 5, root.CanvasPage)
+        # A7
+        if 85 in all_notes:
+            white_key(435+x, 25+y, 5, root.CanvasPage)
+        # a7
+        if 86 in all_notes:
+            black_key(440+x, 15+y, 5, root.CanvasPage)
+        # B7
+        if 87 in all_notes and 88 in all_notes:
+            small_white_key(440+x, 20+y)
+        if 87 in all_notes and 88 not in all_notes:
+            white_key(445+x, 25+y, 5, root.CanvasPage)
+        # C8
+        if 88 in all_notes and 87 in all_notes:
+            small_white_key(447.5+x, 20+y)
+        if 88 in all_notes and 87 not in all_notes:
+            white_key(450+x, 25+y, 5, root.CanvasPage)
